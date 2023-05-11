e09a2c73ea6fbac9db289d47c09f10c97b8d568c
Ilya Shevelev <robotedu@yandex.ru>
2023-05-11 22:25:29 +0300

    Initial commit

---
+++ requests_API_http1.py
@@ -0,0 +1,24 @@
+import requests
+
+
+class Superhero_API:
+
+    def get_statistics(self):
+        url = "https://akabab.github.io/superhero-api/api/all.json"
+        response = requests.get(url, headers={'User-agent': 'netology'})
+        return response.json()
+def get_heroes_powerstats(heroes_list, stat_name) -> dict:
+    superhero_API = Superhero_API()
+    data_all = superhero_API.get_statistics()
+    heroes_stat = {}
+    for superhero in heroes_list:
+        [heroes_stat.setdefault(hero['name'], hero['powerstats'][stat_name]) for hero in data_all if hero['name'] == superhero]
+    return heroes_stat
+
+if __name__ == '__main__':
+    heroes = ['Hulk', 'Captain America', 'Thanos']
+    stat = 'intelligence'
+
+    heroes_dict = get_heroes_powerstats(heroes, stat)
+    res = sorted(heroes_dict.items(), key=lambda item: item[1], reverse=True)
+    print(res[0][0])
